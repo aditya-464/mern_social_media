bbe3697f97db5e94478a3a765957a1e28b0a98a5
Aditya <adityagiri1911@gmail.com>
2023-07-19 20:18:47 +0530

    trial image slider

--- client/src/pages/Ad.jsx
+++ client/src/pages/Ad.jsx
@@ -1,8 +1,38 @@
-import ImageSlider from 'components/ImageSlider'
+// import ImageSlider from 'components/ImageSlider'
 import React from 'react'
+import img1 from "../assets/ok1.webp"
+import img3 from "../assets/ok3.webp"
+import img4 from "../assets/ok4.webp"
+import img6 from "../assets/ok6.webp"
+import img8 from "../assets/ok8.webp"
+import SimpleImageSlider from "react-simple-image-slider";
+import { Box } from '@chakra-ui/react'
 
 export const Ad = () => {
+  const images = [
+    { url: img1 },
+    { url: img3 },
+    { url: img4 },
+    { url: img6 },
+    { url: img8 },
+    // { url: img1 },
+    // { url: img1 },
+  ];
+
   return (
-    <ImageSlider></ImageSlider>
+    <>
+      <Box 
+      width={"20vw"}
+      >
+        <SimpleImageSlider
+          width={560}
+          height={500}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+        />
+      </Box>
+    </>
+    // <ImageSlider></ImageSlider>
   )
 }
