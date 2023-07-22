752e9283dbfd9bcfa89466587787f744f2667e2a
Aditya <adityagiri1911@gmail.com>
2023-07-22 22:34:08 +0530

    Firebase used to upload picture in create post route without env

--- server/index.js
+++ server/index.js
@@ -32,13 +32,20 @@ dotenv.config({ path: "./config.env" });
 
 // Firebase config
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAcBplFz9ol6t35GxdHCJZJgYMkfZF3IJw",
+    authDomain: "vakya-mern.firebaseapp.com",
+    projectId: "vakya-mern",
+    storageBucket: "vakya-mern.appspot.com",
+    messagingSenderId: "793614091966",
+    appId: "1:793614091966:web:11f09da1b1937f08a400ef",
+    measurementId: "G-T25SD2XPH3"
+    // apiKey: process.env.FIREBASE_API_KEY,
+    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.FIREBASE_APP_ID,
+    // measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage();
